767baff1b3ac4ccca78f0cd912e88b6f573ae3b5
Brian Kim <briankim1512@gmail.com>
2017-07-16 01:03:28 +0900

    Added additional code to complete challenges 1-4 in webserver creation course

--- vagrant/proto/webServer.py
+++ vagrant/proto/webServer.py
@@ -12,35 +12,11 @@ session = DBSession()
 class webserverHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		try:
-			if self.path.endswith("/hello"):
-				self.send_response(200)
-				self.send_header('Content-type', 'text/html')
+			if self.path.endswith("/"):
+				self.send_response(301)
+				self.send_header('Location', '/restaurant')
 				self.end_headers()
 
-				output = ""
-				output += "<html><body>Hello!"
-				output += "<form method='POST' enctype='multipart/form-data' action='/hello'>\
-                                <h2>What would you like me to say?</h2><input name='message' type='text'>\
-                                <input type='submit' value='Submit'></form>"
-				output += "</body></html>"
-				self.wfile.write(output)
-				print output
-				return
-
-			if self.path.endswith("/hola"):
-                                self.send_response(200)
-                                self.send_header('Content-type', 'text/html')
-                                self.end_headers()
-
-                                output = ""
-                                output += "<html><body>Hola!"
-				output += "<form method='POST' enctype='multipart/form-data' action='/hello'>\
-                                <h2>What would you like me to say?</h2><input name='message' type='text'>\
-                                <input type='submit' value='Submit'></form>"
-				output += "</body></html>"
-                                self.wfile.write(output)
-                                print output
-                                return
 			if self.path.endswith("/restaurant"):
 				self.send_response(200)
 				self.send_header('Content-type', 'text/html')
@@ -49,9 +25,27 @@ class webserverHandler(BaseHTTPRequestHandler):
 				output = ""
 				tempq = ""
 				output += "<html><body>"
+				output += "<a href='/restaurant/new'><h1>Make a new restaurant</h1></a>"
 				for name in session.query(Restaurant.name):
-					tempq = tempq+str(name[0])+"<br>"	
-				output += tempq
+					output += str(name[0])+"<br>"\
+					+"<a href='#'>Edit</a><br>"\
+					+"<a href='#'>Delete</a><br>"	
+				self.wfile.write(output)
+				print output
+				return
+			
+			if self.path.endswith("/restaurant/new"):
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+
+				output = ""
+				tempq = ""
+				output += "<html><body>"
+				output += "<form method='POST' enctype='multipart/form-data' action='/restaurant/new'>\
+				           <h2>What is the new restaurant name?</h2><input name='message' type='text'>\
+						   <input type='submit' value='Submit'></form>"
+				output += "</body></html>"
 				self.wfile.write(output)
 				print output
 				return
@@ -61,24 +55,20 @@ class webserverHandler(BaseHTTPRequestHandler):
 
 	def do_POST(self):
 		try:
-			self.send_response(301)
-			self.end_headers()
+			if self.path.endswith("/restaurant/new"):
+				self.send_response(301)
 
-			ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-			if ctype == 'multipart/form-data':
-				fields = cgi.parse_multipart(self.rfile, pdict)
-				messagecontent = fields.get('message')
+				ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+				if ctype == 'multipart/form-data':
+					fields = cgi.parse_multipart(self.rfile, pdict)
+					messagecontent = fields.get('message')
+			
+				newRestaurant = Restaurant(name=messagecontent[0])
+				session.add(newRestaurant)
+				session.commit()
 
-			output = ""
-			output += "<html><body>"
-			output += "<h2>Okay, how about this: </h2>"
-			output += "<h1>%s</h1>" % messagecontent[0]
-			output += "<form method='POST' enctype='multipart/form-data' action='/hello'>\
-				<h2>What would you like me to say?</h2><input name='message' type='text'>\
-				<input type='submit' value='Submit'></form>"
-			output += "</body></html>"
-			self.wfile.write(output)
-			print output
+				self.send_header("Location", "/restaurant")
+				self.end_headers()
 
 		except:
 			pass
